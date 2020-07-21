919aa0fba9bdb1057427dc3c822a0b433f6ce02e
Bradley Caravana <bradleycaravana@gmail.com>
2020-07-21 11:44:42 -0400

    Added yahoo email instead of gmail for nodemailer.

--- routes/contact.js
+++ routes/contact.js
@@ -25,12 +25,12 @@ route.post("/send", async (req, res) => {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.mail.yahoo.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: process.env.GOOGLE_EMAIL, // generated ethereal user
-      pass: process.env.GOOGLE_PASSWORD, // generated ethereal password
+      user: process.env.YAHOO_EMAIL, // generated ethereal user
+      pass: process.env.YAHOO_PASSWORD, // generated ethereal password
     },
     tls: {
       rejectUnauthorized: false,
@@ -39,13 +39,22 @@ route.post("/send", async (req, res) => {
 
   // send mail with defined transport object
   try {
-    let info = await transporter.sendMail({
-      from: `"Nodemailer Contact 🔥" ${process.env.GOOGLE_EMAIL}`, // sender address
-      to: "bradleycaravana@gmail.com", // list of receivers
-      subject: "Node Contact Request", // Subject line
-      text: "Hello world?", // plain text body
-      html: output, // html body
-    });
+    let info = await transporter.sendMail(
+      {
+        from: `"Nodemailer Contact 🔥" ${process.env.YAHOO_EMAIL}`, // sender address
+        to: "bradleycaravana@gmail.com", // list of receivers
+        subject: "Node Contact Request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output, // html body
+      },
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Your message has been sent!");
+        }
+      }
+    );
   } catch (err) {
     console.log(err);
   }
