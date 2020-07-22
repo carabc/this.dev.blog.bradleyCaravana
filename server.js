bb9b2e8f4cfc2af6a67135a70866019073da29e9
Bradley Caravana <bradleycaravana@gmail.com>
2020-07-21 23:39:36 -0400

    Passport OAuth20 fixing

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const fileupload = require("express-fileupload");
 const methodOverride = require("method-override");
 const errorHandler = require("./middleware/error");
 const initPassport = require("./config/passport");
+const User = require("../models/User");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 // Init express
 const app = express();
@@ -28,7 +30,45 @@ connectDB();
 app.use(express.static(path.join(__dirname, "/public")));
 
 // Passport config
-app.use(initPassport(passport));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          done(null, user);
+        } else {
+          const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value,
+          };
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
 
 // Handlebars helpers
 const {
