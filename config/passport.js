a32dafb9b96159df2d70b6ea6d51ac89502b77d8
Bradley Caravana <bradleycaravana@gmail.com>
2020-07-22 00:07:58 -0400

    adjusted callback url for OAuth20

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ async function initPassport(passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "http://bradleycaravana.com/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
