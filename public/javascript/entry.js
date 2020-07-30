432ae4ace7fe8a0cfe0fc5e2d615b30ab3535729
Bradley Caravana <bradleycaravana@gmail.com>
2020-07-30 18:10:33 -0400

    Set dark mode in local storage.

--- public/javascript/entry.js
+++ public/javascript/entry.js
@@ -19,8 +19,32 @@ navContainer.addEventListener("click", (e) => {
 
 // Wire up the button to toggle dark mode when clicked with some JavaScript
 const darkButton = document.querySelector(".dark-toggle");
+let darkMode = localStorage.getItem("darkMode");
 
-darkButton.addEventListener("click", (e) => {
-  // When the button is clicked, toggle the class of 'dark' on the body's classList
-  document.body.classList.toggle("dark");
+// darkButton.addEventListener("click", (e) => {
+//   // When the button is clicked, toggle the class of 'dark' on the body's classList
+//   document.body.classList.toggle("dark");
+// });
+
+const enableDarkMode = () => {
+  document.body.classList.add("dark");
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark");
+  localStorage.setItem("darkMode", "null");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkButton.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 });
